35dd30c8f313c46aaeaeb2d9b20c279c46f28a97
Tomas Zuran <tomas.zuran@gmail.com>
2023-09-01 21:32:59 -0300

    Third task completed

--- src/task3.cpp
+++ src/task3.cpp
@@ -5,6 +5,32 @@ using namespace std;
 
 int main(int argv, char ** argc)
 {
+    string adn;
+    cin >> adn;
+
+    char prev = 0;
+    uint64_t max  = 0, current = 0;
+
+    for(char c : adn)
+    {
+        if(c == prev)
+        {
+            current++;
+        }
+        else
+        {
+            if(current > max)
+                max = current;
+            
+            prev = c;
+            current = 1;
+        }
+    }
+
+    if(current > max)
+        max = current;
+
+    cout << max;
 
     return 0;
 }
